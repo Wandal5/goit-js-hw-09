f34c4210422defa64367fb5a1e8f03c8db46ec9d
Wandal5 <100094775+Wandal5@users.noreply.github.com>
2022-11-16 22:30:35 +0200

    3

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+function getBgColor() {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+}
+
+function toggleDisabled() {
+  dataStartRef.toggleAttribute('disabled');
+  dataStopRef.toggleAttribute('disabled');
+}
+
+let timerId;
+
+function onStart(e) {
+  timerId = setInterval(getBgColor, 1000);
+  toggleDisabled();
+}
+
+function onStop() {
+  clearInterval(timerId);
+  toggleDisabled();
+}
+
+const dataStartRef = document.querySelector('[data-start]');
+const dataStopRef = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+
+dataStartRef.addEventListener('click', onStart);
+dataStopRef.addEventListener('click', onStop);
